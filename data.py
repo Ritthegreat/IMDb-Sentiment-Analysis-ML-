1753538826061b4fdb43402ea06bb0241f133aed
Siddharth Ajay <sid.ajay05@gmail.com>
2025-06-02 18:59:35 -0700

    logistic

--- data.py
+++ data.py
@@ -1,15 +1,30 @@
 import tensorflow_datasets as tfds
 import numpy as np
+import os, pickle
 
 
-def load_dataset(name: str):
+# cache waste hella storage
+def load_dataset(name: str, cache_dir="cache", cache=False):
     train_data, test_data = tfds.load(
-        name,
-        split=["train", "test"],
-        as_supervised=True,
+        name, split=["train", "test"], as_supervised=True, data_dir=cache_dir
     )
-    return tfds_as_np(train_data), tfds_as_np(test_data)
+    if not cache:
+        return tfds_as_np(train_data), tfds_as_np(test_data)
+
+    if not os.path.exists("cache/train_np.pkl"):
+        with open("cache/train_np.pkl", "wb") as f:
+            pickle.dump(tfds_as_np(train_data), f)
+    if not os.path.exists("cache/test_np.pkl"):
+        with open("cache/test_np.pkl", "wb") as f:
+            pickle.dump(tfds_as_np(test_data), f)
+    with open("cache/train_np.pkl", "rb") as f:
+        train_np = pickle.load(f)
+    with open("cache/test_np.pkl", "rb") as f:
+        test_np = pickle.load(f)
+    return train_np, test_np
 
 
 def tfds_as_np(ds):
-    return np.array([(text.decode("utf-8"), label) for text, label in tfds.as_numpy(ds)])
+    return np.array(
+        [(text.decode("utf-8"), label) for text, label in tfds.as_numpy(ds)]
+    )
--- logistic.py
+++ logistic.py
@@ -1,15 +1,20 @@
 from data import load_dataset
 from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import accuracy_score
 
-train_data, test_data = load_dataset("imdb_reviews")
-
-vectorizer = CountVectorizer(stop_words="english")
+train_data, test_data = load_dataset("imdb_reviews", cache=True)
+vectorizer = TfidfVectorizer(stop_words="english")
 X_tr = vectorizer.fit_transform(train_data[:, 0])
 y_tr = train_data[:, 1]
-X_te = vectorizer.fit_transform(test_data[:, 0])
+X_te = vectorizer.transform(test_data[:, 0])
 y_te = test_data[:, 1]
-print(X_tr)
 
-model = LogisticRegression()
+model = LogisticRegression(solver="liblinear", penalty="l2", C=1)
 model.fit(X_tr, y_tr)
+y_pred_tr = model.predict(X_tr)
+y_pred = model.predict(X_te)
+tr_acc = accuracy_score(y_pred_tr, y_tr)
+te_acc = accuracy_score(y_pred, y_te)
+print(f"Train Acc: {tr_acc}")
+print(f"Test Acc: {te_acc}")
